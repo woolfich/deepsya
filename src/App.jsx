749f2ef897e693b8d2d91bd15f9fd6f3c31b221e
Woolfich <peresver@gmail.com>
2025-09-29 20:54:44 +0300

    move deploy.yml to correct folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+// src/App.jsx
+import { Router } from 'preact-router';
+import Main from './pages/Main';
+import Norms from './pages/Norms';
+import Summary from './pages/Summary';
+import WelderCard from './pages/WelderCard';
+
 export default function App() {
-  return <h1 style={{color:'tomato'}}>App работает!</h1>
+  return (
+    <Router url="/deepsya/">
+      <Main path="/" />
+      <Norms path="/norms" />
+      <Summary path="/summary" />
+      <WelderCard path="/welder/:id" />
+    </Router>
+  );
 }
